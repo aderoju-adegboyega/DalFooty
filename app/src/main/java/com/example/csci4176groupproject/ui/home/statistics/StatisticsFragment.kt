ee62ac7a6f03ccc0716f3809ddb55aa25f95fdc0
Chengwen Yang <ch807441@dal.ca>
2023-03-23 02:45:31 -0300

    Stats page & League table, Goals Page, Chengwen

---
+++ app/src/main/java/com/example/csci4176groupproject/ui/home/statistics/StatisticsFragment.kt
@@ -0,0 +1,68 @@
+package com.example.csci4176groupproject.ui.home.statistics
+
+import android.content.DialogInterface
+import android.content.DialogInterface.OnClickListener
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.csci4176groupproject.GoalsActivity
+import com.example.csci4176groupproject.databinding.FragmentStatisticsBinding
+import com.example.csci4176groupproject.leagueActivity
+
+class StatisticsFragment : Fragment() {
+
+    private var _binding: FragmentStatisticsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val statisticsViewModel =
+            ViewModelProvider(this).get(StatisticsViewModel::class.java)
+
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        _binding?.textView5?.setOnClickListener {
+            startActivity(Intent(it.context, leagueActivity::class.java))
+        }
+        _binding?.textView6?.setOnClickListener {
+            startActivity(Intent(it.context, GoalsActivity::class.java))
+        }
+        _binding?.button?.setOnClickListener {
+            AlertDialog.Builder(it.context)
+                .setTitle("Detail")
+                .setPositiveButton("confirm",null)
+                .setItems(arrayOf("Top1 goals","Top2 goals"), object : OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    Toast.makeText(it.context, "click", Toast.LENGTH_SHORT).show()
+                }
+            }).show()
+        }
+        _binding?.button2?.setOnClickListener {
+            AlertDialog.Builder(it.context)
+                .setTitle("Detail")
+                .setPositiveButton("confirm",null)
+                .setItems(arrayOf("Top1 goals","Top2 goals"), object : OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        Toast.makeText(it.context, "click", Toast.LENGTH_SHORT).show()
+                    }
+                }).show()
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
