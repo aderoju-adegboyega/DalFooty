f785a4c3ca3640df11ace56398e4d896222636ff
hhe <hy876033@dal.ca>
2023-03-26 20:33:48 +0800

    myteam

--- app/src/main/java/com/example/csci4176groupproject/ui/myteam/MyteamFragment.kt
+++ app/src/main/java/com/example/csci4176groupproject/ui/myteam/MyteamFragment.kt
@@ -1,14 +1,18 @@
 package com.example.csci4176groupproject.ui.myteam
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.TextUtils
+import android.view.*
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.csci4176groupproject.R
 import com.example.csci4176groupproject.databinding.FragmentMyteamBinding
-import com.example.csci4176groupproject.ui.home.HomeViewModel
+
 
 class MyteamFragment : Fragment() {
 
@@ -33,12 +37,72 @@ class MyteamFragment : Fragment() {
         myteamViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        setHasOptionsMenu(true);
+        initTeamData();
         return root
+    }
+    private var team: String = "Aston Villa"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.spinner.onItemSelectedListener  = object :OnItemSelectedListener{
+
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                activity?.let {
+                    team = it.resources.getStringArray(R.array.teams)[p2]
+                }
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+
+        binding.btnSelect.setOnClickListener {
+            activity?.let {
+                var sp = it.getSharedPreferences("team", Context.MODE_PRIVATE)
+                sp.edit().putString("team",team).commit()
+                initTeamData()
+            }
+        }
+    }
+
+    private fun initTeamData() {
+        activity?.let {
+            var sp = it.getSharedPreferences("team", Context.MODE_PRIVATE)
+            var team = sp.getString("team",team)
+            if (!TextUtils.isEmpty(team)){
+                binding.noTeam.visibility = View.GONE
+                binding.hasTeam.visibility = View.VISIBLE
+            }else{
+                binding.noTeam.visibility = View.VISIBLE
+                binding.hasTeam.visibility = View.GONE
+            }
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.add(0,1001,1,"Change Team")
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==1001){
+            binding.noTeam.visibility = View.VISIBLE
+            binding.hasTeam.visibility = View.GONE
+        }
+        return true
+
+    }
+
+
 
 }
